a5b2ea32028dd43f63a3e8f31063f11d6a66afb2
norma2627 <11norma11norma@gmail.com>
2024-07-11 15:51:00 +0900

    🐧🐧🐧

--- app/main.py
+++ app/main.py
@@ -1,26 +1,27 @@
 import discord  # Pycordを読み込む(pip install py-cord)
+import dotenv
 import os
-from dotenv import load_dotenv
 from server import server_thread
 
 # .envファイルの内容を読み込む
-load_dotenv()
+dotenv.load_dotenv()
 
 # TOKENの読み込み
 TOKEN = os.environ.get('TOKEN')
 
 # botの元オブジェクトを生成
 intents = discord.Intents.default()
-intents.message_content = True  # メッセージ内容を取得する権限を付与
-bot = discord.Client(intents=intents)
+intents.message_content = True
+intents.voice_states = True
+client = discord.Client(intents=intents)
 
 # 起動時に自動的に動くメソッド
-@bot.event
+@client.event
 async def on_ready():
     print("起動完了")  # 起動すると実行したターミナルに"起動完了"と表示される
 
 # メッセージが投稿された時に動くメソッド
-@bot.event
+@client.event
 async def on_message(message: discord.Message):
     # メッセージ送信者がbotであれば無視する
     if message.author.bot:
@@ -30,7 +31,7 @@ async def on_message(message: discord.Message):
         await message.reply("やぁ！")
 
 # /コマンドを実装
-@bot.command(name="test", description="テスト")
+@client.command(name="test", description="テスト")
 async def test(ctx: discord.ApplicationContext):
     await ctx.respond("てすとてすと")
 
@@ -38,20 +39,20 @@ async def test(ctx: discord.ApplicationContext):
 theme_title = None
 
 # テーマを保存
-@bot.command(name="post", description="ボットにテーマを保存")
+@client.command(name="post", description="ボットにテーマを保存")
 async def post(ctx: discord.ApplicationContext, theme: str):
     global theme_title
     theme_title = theme
     await ctx.respond(f"テーマ`{theme}`を保存したよ！")
 
 # テーマを取得
-@bot.command(name="get", description="ボットからテーマを取得")
+@client.command(name="get", description="ボットからテーマを取得")
 async def get(ctx: discord.ApplicationContext):
     global theme_title
     await ctx.respond(f"今週のテーマは`{theme_title}`だよ！")
 
 # テーマの確認
-@bot.command(name="check", description="テーマの確認")
+@client.command(name="check", description="テーマの確認")
 async def check(ctx: discord.ApplicationContext):
     if theme_title is not None:
         await ctx.respond(f"今のテーマは`{theme_title}`だよ！")
@@ -62,4 +63,4 @@ async def check(ctx: discord.ApplicationContext):
 server_thread()
 
 # Discord botを実行
-bot.run(TOKEN)
+client.run(TOKEN)
