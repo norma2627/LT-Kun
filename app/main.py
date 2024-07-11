283de0e476efbec871134fb94cba724ecc8c35b5
norma2627 <11norma11norma@gmail.com>
2024-07-11 15:09:26 +0900

    :octopus::octopus::octopus:

---
+++ app/main.py
@@ -0,0 +1,65 @@
+import discord  # Pycordを読み込む(pip install py-cord)
+import os
+from dotenv import load_dotenv
+from server import server_thread
+
+# .envファイルの内容を読み込む
+load_dotenv()
+
+# TOKENの読み込み
+TOKEN = os.environ.get('TOKEN')
+
+# botの元オブジェクトを生成
+intents = discord.Intents.default()
+intents.message_content = True  # メッセージ内容を取得する権限を付与
+bot = discord.Bot(intents=intents)
+
+# 起動時に自動的に動くメソッド
+@bot.event
+async def on_ready():
+    print("起動完了")  # 起動すると実行したターミナルに"起動完了"と表示される
+
+# メッセージが投稿された時に動くメソッド
+@bot.event
+async def on_message(message: discord.Message):
+    # メッセージ送信者がbotであれば無視する
+    if message.author.bot:
+        return
+    # メッセージが"こんにちは"なら、"やぁ！"と返信する
+    if message.content == "こんにちは":
+        await message.reply("やぁ！")
+
+# /コマンドを実装
+@bot.command(name="test", description="テスト")
+async def test(ctx: discord.ApplicationContext):
+    await ctx.respond("てすとてすと")
+
+# テーマを保存するためのtheme_title変数を用意
+theme_title = None
+
+# テーマを保存
+@bot.command(name="post", description="ボットにテーマを保存")
+async def post(ctx: discord.ApplicationContext, theme: str):
+    global theme_title
+    theme_title = theme
+    await ctx.respond(f"テーマ`{theme}`を保存したよ！")
+
+# テーマを取得
+@bot.command(name="get", description="ボットからテーマを取得")
+async def get(ctx: discord.ApplicationContext):
+    global theme_title
+    await ctx.respond(f"今週のテーマは`{theme_title}`だよ！")
+
+# テーマの確認
+@bot.command(name="check", description="テーマの確認")
+async def check(ctx: discord.ApplicationContext):
+    if theme_title is not None:
+        await ctx.respond(f"今のテーマは`{theme_title}`だよ！")
+    else:
+        await ctx.respond("まだテーマが設定されていないよ！")
+
+# koyeb用サーバ立ち上げ
+server_thread()
+
+# Discord botを実行
+bot.run(TOKEN)
---
+++ app/server.py
@@ -0,0 +1,22 @@
+# Fast-API立ち上げ
+
+from threading import Thread   # スレッド作成のための標準ライブラリ
+from fastapi import FastAPI   # APIサーバを作成するためのフレームワーク
+import uvicorn   # FastAPIアプリケーションを実行するために使用
+
+
+# FastAPIアプリケーションの設定
+app = FastAPI() # アプリケーションインスタンスを作成
+
+@app.get("/")   # ルートエンドポイントの定義
+async def root():
+    return {"message": "サーバーの起動完了"}
+
+# サーバの起動
+def start():
+    uvicorn.run(app, host="0.0.0.0", port=2627)
+
+# サーバを別スレッドで実行
+def server_thread():
+    t = Thread(target=start)
+    t.start()
